4a2aeccd9d29c5212856c5881d3c8ff2e37963e0
MarucoMass <mariomass23@gmail.com>
2024-08-27 00:09:09 -0300

    feature(project.index): sort and direction functions

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,10 +1,47 @@
 import Pagination from "@/Components/Pagination";
+import SelectInput from "@/Components/SelectInput";
+import TextInput from "@/Components/TextInput";
 import { PROJECT_STATUS_TEXT_MAP, PROJECT_STATUS_CLASS_MAP } from "@/constants";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
-import { Head, Link } from "@inertiajs/react";
+import { Head, Link, router } from "@inertiajs/react";
 import React from "react";
 
-const Index = ({ auth, projects }) => {
+const Index = ({ auth, projects, queryParams = null }) => {
+    queryParams = queryParams || {};
+
+    const handleFieldChange = (name, value) => {
+        if (value) {
+            queryParams[name] = value;
+        } else {
+            delete queryParams[name];
+        }
+
+        console.log(queryParams);
+
+        router.get(route("project.index"), queryParams);
+    };
+
+    const handleNameSearch = (name, e) => {
+        if (e.key !== "Enter") return;
+
+        handleFieldChange(name, e.target.value);
+    };
+
+    const handleOrder = (name) => {
+        if(name === queryParams.sort_field){
+            if (queryParams.sort_direction === "asc") {
+                queryParams.sort_direction = "desc";
+            } else {
+                queryParams.sort_direction = "asc";
+            }
+        } else {
+            queryParams.sort_field = name;
+            queryParams.sort_direction = "asc";
+        }
+
+        router.get(route("project.index"), queryParams);
+    }
+
     return (
         <Authenticated
             user={auth.user}
@@ -22,20 +59,102 @@ const Index = ({ auth, projects }) => {
                             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                 <thead className="text-base text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
                                     <tr className="text-nowrap">
-                                        <th className="px-3 py-2">id</th>
+                                        <th
+                                            onClick={(e) => handleOrder("id")}
+                                            className="px-3 py-2"
+                                        >
+                                            id
+                                        </th>
                                         <th className="px-3 py-2">image</th>
-                                        <th className="px-3 py-2">name</th>
-                                        <th className="px-3 py-2">status</th>
-                                        <th className="px-3 py-2">
+                                        <th
+                                            onClick={(e) => handleOrder("name")}
+                                            className="px-3 py-2"
+                                        >
+                                            name
+                                        </th>
+                                        <th
+                                            onClick={(e) =>
+                                                handleOrder("status")
+                                            }
+                                            className="px-3 py-2"
+                                        >
+                                            status
+                                        </th>
+                                        <th
+                                            onClick={(e) =>
+                                                handleOrder("created_at")
+                                            }
+                                            className="px-3 py-2"
+                                        >
                                             created date
                                         </th>
-                                        <th className="px-3 py-2">due date</th>
+                                        <th
+                                            onClick={(e) =>
+                                                handleOrder("due_date")
+                                            }
+                                            className="px-3 py-2"
+                                        >
+                                            due date
+                                        </th>
                                         <th className="px-3 py-2">
                                             created by
                                         </th>
                                         <th className="px-3 py-2 text-right">
                                             actions
                                         </th>
+                                    </tr>
+                                </thead>
+                                <thead className="text-base text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                    <tr className="text-nowrap">
+                                        <th className="px-3 py-2"></th>
+                                        <th className="px-3 py-2"></th>
+                                        <th className="px-3 py-2">
+                                            <TextInput
+                                                defaultValue={queryParams.name}
+                                                className="w-full text-white placeholder-shown:text-white/55"
+                                                placeholder="Enter the name"
+                                                onBlur={(e) =>
+                                                    handleFieldChange(
+                                                        "name",
+                                                        e.target.value
+                                                    )
+                                                }
+                                                onKeyPress={(e) =>
+                                                    handleNameSearch("name", e)
+                                                }
+                                            ></TextInput>
+                                        </th>
+                                        <th className="px-3 py-2">
+                                            <SelectInput
+                                                defaultValue={
+                                                    queryParams.status
+                                                }
+                                                className="w-full"
+                                                onChange={(e) =>
+                                                    handleFieldChange(
+                                                        "status",
+                                                        e.target.value
+                                                    )
+                                                }
+                                            >
+                                                <option value="">
+                                                    Select status
+                                                </option>
+                                                <option value="pending">
+                                                    Pending
+                                                </option>
+                                                <option value="in_progress">
+                                                    In progress
+                                                </option>
+                                                <option value="completed">
+                                                    Completed
+                                                </option>
+                                            </SelectInput>
+                                        </th>
+                                        <th className="px-3 py-2"></th>
+                                        <th className="px-3 py-2"></th>
+                                        <th className="px-3 py-2"></th>
+                                        <th className="px-3 py-2 "></th>
                                     </tr>
                                 </thead>
                                 <tbody className="text-base text-gray-700 uppercase bg-gray-50 dark:bg-transparent dark:text-gray-400 border-b-2 border-gray-500">
